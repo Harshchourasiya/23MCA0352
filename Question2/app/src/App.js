08ddecb0be08c291f1e9c9c95f79fca9d610754e
unknown <Administrator@CDC717-096.VITUNIVERSITY.LOCAL>
2024-03-30 18:01:21 +0530

    Final Submit 2

--- Question2/app/src/App.js
+++ Question2/app/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState, useEffect} from 'react'
 function App() {
+  const [data, setData] = useState();
+  useEffect(async()=> {
+    const data1 = await fetch("http://localhost:3000/categories/Computer/product?n=10&sortBy=price&orderBy=DES&page=3");
+    const jData = await data1.json();
+    setData(jData);
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        data.map((d,k)=> {
+          return (
+            <div>
+              <h1>{d.productName}</h1>
+              <p>{p.price}</p>
+              <p>{p.discount}</p>
+              <p>{p.rate}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
